bbf2b85ec0b7ff7d42bf925d76702e38becb0eaa
YC815 <minnecraft.YC815@gmail.com>
2023-12-10 00:57:44 +0800

    成功上傳

--- py/main.py
+++ py/main.py
@@ -12,23 +12,28 @@ from openai import OpenAI
 import re
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from instagrapi import Client
 
 load_dotenv()
-client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+client = OpenAI(api_key=os.getenv('API_KEY'))
 
-
-def wait_for_clickable(driver, locator, timeout):
-    try:
-        WebDriverWait(driver, timeout, 1).until(
-            EC.element_to_be_clickable(locator))
-        # return True
-    except TimeoutException:
-        print("Wait TimeoutException:%s" % str(locator))
-        # return False
-
+# 用戶和AI的訊息
+user = "第四次測試"
+ai_response = client.chat.completions.create(model="gpt-3.5-turbo", messages=[
+    {"role": "user", "content": "請在68字內簡單幽默的回答\"%s\"。" % (user)}
+])
+ai_response = re.search(r"content='(.*?)', role", str(ai_response))
+ai = str(ai_response.group(1))
+user = user.replace('\n', ' ')  # 將換行符替換為空格
+user = [user[i:i + 14] for i in range(0, len(user), 14)]
+user = "\n".join(user)
+print(ai_response)
+ai = ai.replace('\n', ' ')  # 將換行符替換為空格
+ai = [ai[i:i + 17] for i in range(0, len(ai), 17)]
+ai = "\n".join(ai)
 
 # 打開圖片
-image = Image.open('public/ins.png')
+image = Image.open('public/ins.jpg')
 draw = ImageDraw.Draw(image)
 
 # 加載一個支持中文的字體
@@ -45,39 +50,21 @@ text_position_ai = (50, 700)  # 調整文字的位置
 draw.text(text_position_ai, ai, fill=text_color, font=font)
 
 # 保存圖片
-image.save('public/ready.png')
+image.load()
+background = Image.new("RGB", image.size, (255, 255, 255))
+background.paste(image, mask=image.split()[3])  # 3 is the alpha channel
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+background.save('public/ready.jpg', 'JPEG', quality=80)
+cl = Client()
+cl.login(os.getenv('USERNAME'), os.getenv('PASSWORD'))
+media = cl.photo_upload(
+    "public/ready.jpg",
+    "濱江匿名網5.0",
+    extra_data={
+        "custom_accessibility_caption": "alt text example",
+        "like_and_view_counts_disabled": 0,
+        "disable_comments": 0,
+    }
+)
 
-driver.get("https://www.instagram.com")
-
-
-wait = WebDriverWait(driver, 10)
-
-element_xpath = "/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[1]/div/label/input"
-element = wait.until(lambda driver: driver.find_element(By.XPATH, element_xpath))
-element.send_keys(os.getenv("USERNAME"))
-
-element_xpath = "/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[2]/div/label/input"
-element = wait.until(lambda driver: driver.find_element(By.XPATH, element_xpath))
-element.send_keys(os.getenv("PASSWORD"))
-
-element_xpath = "/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]"
-element = wait.until(lambda driver: driver.find_element(By.XPATH, element_xpath))
-element.click()
-
-
-wait = WebDriverWait(driver, 10)
-
-element_xpath = "/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[7]/div/span/div/a/div"
-element = wait.until(lambda driver: driver.find_element(By.XPATH, element_xpath))
-element.click()
-
-wait = WebDriverWait(driver, 10)
-element_xpath = "/html/body/div[10]/div[1]/div/div[3]/div/div/div/div/div/div/div/div[2]/div[1]/div/div/div[2]/div/button"
-
-wait_for_clickable(driver, (By.XPATH, element_xpath), 10)
-imgInput = driver.find_element(By.XPATH, element_xpath)
-imgInput.send_keys('public/ready.png')
-
-driver.quit()
+media.dict()
--- py/make_img.py
+++ py/make_img.py
@@ -1,40 +1,9 @@
-from PIL import Image, ImageDraw, ImageFont
-from openai import OpenAI
+from instabot import Bot
 
-client = OpenAI(api_key='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')
+bot = Bot()
 
+bot.login(username="bjjh.niming.5", password="BJNIMING")
 
+bot.upload_photo("public/ready.png", caption="")
 
-# 用戶和AI的訊息
-user = "笑死今天好累"
-ai_response = client.chat.completions.create(model="gpt-3.5-turbo",
-messages=[
-    {"role": "user", "content": "請在68字內簡單幽默的回答\"%s\"。" % (user)}
-])
-user = user.replace('\n', ' ')  # 將換行符替換為空格
-user = [user[i:i + 14] for i in range(0, len(user), 14)]
-user = "\n".join(user)
-
-ai = ai_response.choices[0].message["content"].replace('\n', ' ')  # 將換行符替換為空格
-ai = [ai[i:i + 17] for i in range(0, len(ai), 17)]
-ai = "\n".join(ai)
-
-# 打開圖片
-image = Image.open('public/ins.png')
-draw = ImageDraw.Draw(image)
-
-# 加載一個支持中文的字體
-font_path = "public/NotoSansTC-Regular.ttf"  # 替換成你的中文字體路徑
-font = ImageFont.truetype(font_path, size=66)  # 設置字體大小
-
-# 繪制文本
-text_position_user = (50, 66)  # 調整文字的位置
-text_color = 'rgb(255, 255, 255)'  # 設定文字顏色
-draw.text(text_position_user, user, fill=text_color, font=font)
-
-font = ImageFont.truetype(font_path, size=56)
-text_position_ai = (50, 700)  # 調整文字的位置
-draw.text(text_position_ai, ai, fill=text_color, font=font)
-
-# 保存圖片
-image.save('public/ready.png')
+bot.logout()
